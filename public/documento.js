08f5ae293c91db421653923191c1398caeb8967c
Dirceusljr <dirceusljr@gmail.com>
2024-06-24 17:42:21 -0300

    criando eventos de front e eventos do socket.io

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,11 @@
-const socket = io(); 
+const socket = io();
+
+const textoEditor = document.getElementById('editor-texto');
+
+textoEditor.addEventListener('keyup', () => {
+    socket.emit('editor_texto', textoEditor.value)
+})
+
+socket.on('editor_texto_cliente', (texto) => {
+    textoEditor.value = texto
+})
