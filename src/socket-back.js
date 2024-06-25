114305eae072251edc3372e73cd41fc11443a8d6
Dirceusljr <dirceusljr@gmail.com>
2024-06-24 21:44:26 -0300

    refatorando + atualizando texto a partir do banco

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,32 +1,24 @@
 import 'dotenv/config.js';
-import { documentosColecao } from "./dbConnect.js"
 import io from "./server.js"
+import { atualizaDocumento, encontrarDocumento } from './documentosDb.js';
 
 io.on('connection', (socket) => {
     console.log('Um cliente se conectou com o servidor! ID:', socket.id)
 
     socket.on('selecionar_documento', async (nomeDocumento, devolverTexto) => {
         socket.join(nomeDocumento)
+        
         const documento = await encontrarDocumento(nomeDocumento)
-        console.log(documento)
         if (documento) {
             devolverTexto(documento.texto)
         }
-
     })
 
-    socket.on('editor_texto', ({ texto, nomeDocumento }) => {
-        const documento = encontrarDocumento(nomeDocumento);
-        if (documento) {
-            documento.texto = texto;
+    socket.on('editor_texto', async ({ texto, nomeDocumento }) => {
+        const atualizacao = await atualizaDocumento(nomeDocumento, texto)
+        if (atualizacao) {
             socket.to(nomeDocumento).emit('editor_texto_cliente', texto)
         }
     })
 })
 
-function encontrarDocumento(nome) {
-    const documento = documentosColecao.findOne({
-        nome
-    })
-    return documento;
-}
